cc7177ac8fdeb4b322e09ecc67e23d92ce73f1df
Your Name <you@example.com>
2018-02-18 19:21:42 +0500

    fixes

--- lab-6.sh
+++ lab-6.sh
@@ -3,8 +3,29 @@ clear
 tput civis -- invisible
 
 declare -A field
-rows=35
-columns=20
+rows=10
+columns=35
+
+while [ "$1" != "" ]
+do
+   case "$1" in
+      max)
+         shift
+         IFS='x' read -r -a dimensionsArray <<< "$1"
+         if [[ ${dimensionsArray[0]} =~ ^-?[0-9]+$ ]] && [[ ${dimensionsArray[1]} =~ ^-?[0-9]+$ ]] ; then
+            rows=${dimensionsArray[0]}
+            columns=${dimensionsArray[1]}
+            break
+         fi
+
+         echo $'\e[31;1m☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠'
+         echo "Syntax Error! Try max 32x32 to change dimensions"
+         exit 1;;
+   esac
+
+   shift
+done
+
 
 playerI=3
 playerJ=6
@@ -15,13 +36,13 @@ lastDimension="w"
 
 for ((i=1; i<=rows; i++)) do
     for ((j=1; j<=columns; j++)) do
-      # field[$i,$j]=$(( $RANDOM % 10 + 0 ))
-      field[$i,$j]="1"
+      field[$i,$j]=$(( $RANDOM % 20 + 0 ))
+      # field[$i,$j]="1"
     done
 done
 
 function generateField () {
-   clear   
+      
    
    # Change postiton
    oldPlayerI="$playerI"
@@ -51,7 +72,7 @@ function generateField () {
       newObjectJ=$((playerJ))
    fi
 
-   if [ "${field[$playerI,$playerJ]}" = "0" ] || (($playerI <= 0)) || (($playerJ <= 0)) ; then
+   if [ "${field[$playerI,$playerJ]}" = "0" ] || [ "${field[$playerI,$playerJ]}" = "-1" ] || (($playerI <= 0)) || (($playerJ <= 0)) || (($playerI >= rows)) || (($playerJ >= columns)) ; then
       newObjectI=$((playerI))
       newObjectJ=$((playerJ))
 
@@ -61,54 +82,64 @@ function generateField () {
 
    # Draw/remove object 
    if [ "$2" = true ] ; then 
-      if [ "${field[$newObjectI,$newObjectJ]}" != "0" ] ; then 
-         field[$newObjectI,$newObjectJ]="0"
+      if [ "${field[$newObjectI,$newObjectJ]}" != "-1" ] ; then 
+         field[$newObjectI,$newObjectJ]="-1"
       else
          field[$newObjectI,$newObjectJ]="1"
       fi
    fi
 
    # Draw player
+   printf "\033c"
+   tput civis -- invisible
+
    for ((i=1; i<=rows; i++)) do
+      output=''
        for ((j=1; j<=columns; j++)) do
          if [ "$i" -eq "$playerI" ] && [ "$j" -eq "$playerJ" ] ; then
-            printf "\e[31;1m🚀"
-            generatedUser=true
+            toPrint=$'\e[1;33;1m🚀'
+            output="$output$toPrint"
+
             continue
          fi
 
-         if [ "${field[$i,$j]}" != "0" ] && [ "${field[$i,$j]}" != "13" ] ; then
-            printf " "
-         elif [ "${field[$i,$j]}" = "13" ] ; then
-            printf "\e[1;32m🌎"
+         if [ "${field[$i,$j]}" -eq "-1" ] ; then
+            toPrint=$'\e[0;32;3m🌍'
+            output="$output$toPrint"
+
+            continue
+         fi
+
+         if [ "${field[$i,$j]}" != "0" ] ; then
+            toPrint=$'\e[40;1m '
+            output="$output$toPrint"
          else 
-            printf "\e[37;3m★"
+            toPrint=$'\e[1;37;3m⭐'
+            output="$output$toPrint"
          fi
        done
-       echo
+
+       echo $output
    done   
 }
 
 # Rendering a main menu
-# clear
-# echo "
-# *****************************************************
-# Developed By Evegeny Dovgiy and Nikita Pavlyuk KE-484
+echo "
+*****************************************************
+Developed By Evegeny Dovgiy and Nikita Pavlyuk KE-484
 
-# Keys:
+Keys:
 
-#  W - UP
-#  S - DOWN
-#  A - LEFT
-#  D - RIGHT
-#  SPACE - pick/put blocks
-#  X - QUIT
+ W - UP
+ S - DOWN
+ A - LEFT
+ D - RIGHT
+ E - pick/put blocks
+ X - QUIT
 
-# Press Enter to continue
-# *****************************************************
-# "
-
-generateField
+Press Enter to continue
+*****************************************************
+"
 
 while :
 do
@@ -119,8 +150,10 @@ do
    s) generateField "s" ;;
    d) generateField "d" ;;
    e) generateField "" true ;;
+   
+   *) generateField;;
 
-   *)   
+   x)   
       tput cnorm   -- normal
       exit 0;;
    esac
